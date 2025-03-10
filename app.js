ff44eecb6fc013f7869f602fadb2a0137ef0ab49
Shaik Abdul Subahan <shaikabdulsubahan01@gmail.com>
2025-03-10 15:58:46 +0530

    Updated the converters

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-function convert(){
-    let cm = document.getElementById('cm');
-    let m = cm.value * 100;
-    let result = 'The converted value is:\n' + cm.value + 'm' + '=' + m + 'cm';
+function convert() {
+    let value = parseFloat(document.getElementById('inputValue').value);
+    let conversionType = document.getElementById('conversionType').value;
+    let result = '';
+
+    if (!isNaN(value)) {
+        switch (conversionType) {
+            case 'metersToCm':
+                result = value + ' meters = ' + (value * 100) + ' centimeters';
+                break;
+            case 'cmToMeters':
+                result = value + ' centimeters = ' + (value / 100) + ' meters';
+                break;
+            case 'kmToMeters':
+                result = value + ' kilometers = ' + (value * 1000) + ' meters';
+                break;
+            case 'milesToKm':
+                result = value + ' miles = ' + (value * 1.60934) + ' kilometers';
+                break;
+        }
+    } else {
+        result = 'Please enter a valid number.';
+    }
+
     document.getElementById('result').innerText = result;
 }
 
 function reset() {
-    document.getElementById('cm').value = '';
+    document.getElementById('inputValue').value = '';
     document.getElementById('result').innerText = 'Here you will get the result!';
 }
